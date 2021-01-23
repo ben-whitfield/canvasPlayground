c5aab62b3d4e66e818af70d09e13931bd7a5722d
benwdev <bwhitfield82@gmail.com>
2021-01-23 21:56:17 +0000

    generate multiple coloured circles

--- scripts/animatedCircles.js
+++ scripts/animatedCircles.js
@@ -4,6 +4,14 @@ function updateCanvasSize(canvas) {
   canvas.height = rect.height;
 }
 
+function getCanvasSize(canvas) {
+  var rect = canvas.parentNode.getBoundingClientRect();
+  return {
+   width: Math.floor(rect.width),
+   height: Math.floor(rect.width)
+  }
+}
+
 function randomNumber(min, max){
   const r = Math.random()*(max-min) + min
   return Math.floor(r)
@@ -23,29 +31,42 @@ function drawCircle(ctx, params) {
 }
 
 
-
-function animatedCircles() {
-  canvas = document.getElementById("target-canvas");
-  updateCanvasSize(canvas)
-  
-  console.log(`Make colour ${generateColour()}`)
-  
+function getCanvasContext(canvas) {
   if (canvas.getContext) {
-    let ctx = canvas.getContext("2d"); 
-    let colour = generateColour()
-    const params = {
-      x: 75, 
-      y: 75, 
-      radius: 75, 
-      startAngle: 0,
-      endAngle: Math.PI*2,
-      antiClockwise: false,
-      colour: colour
-    }
-
-    drawCircle(ctx, params)
-
+    return canvas.getContext("2d");
   }
 }
 
-animatedCircles()
+
+function generateCircleValues(canvas) {
+  const canvasSize = getCanvasSize(canvas)
+  const colour = generateColour()
+  return {
+    x: randomNumber(0, canvasSize.width), 
+    y: randomNumber(0, canvasSize.height), 
+    radius: 75, 
+    startAngle: 0,
+    endAngle: Math.PI*2,
+    antiClockwise: false,
+    colour: colour
+  }
+}
+
+function getCanvas() {
+  return document.getElementById("target-canvas");
+}
+
+function getContext(canvas) {
+  updateCanvasSize(canvas)
+  return getCanvasContext(canvas)
+}
+
+function go(ctx, canvas) {
+  console.log('ran')
+  let params = generateCircleValues(canvas)
+  drawCircle(ctx, params)  
+}
+
+const canvas = getCanvas()
+const ctx = getContext(canvas)
+setInterval(() => go(ctx, canvas), 50);
